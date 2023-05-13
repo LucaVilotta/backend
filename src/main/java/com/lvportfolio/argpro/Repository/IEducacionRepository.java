3cf2bbeb0b5b34470449d7e28f9773e96297ce75
LucaVilotta <lucavilotta@hotmail.com>
2023-05-12 21:33:18 -0300

    Borre toda la educacion

--- src/main/java/com/lvportfolio/argpro/Repository/IEducacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lvportfolio.argpro.Repository;
-
-import com.lvportfolio.argpro.Entity.Educacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IEducacionRepository extends JpaRepository<Educacion, Long> {
-}
