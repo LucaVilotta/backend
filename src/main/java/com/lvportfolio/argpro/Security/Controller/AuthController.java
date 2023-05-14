b037aee9b803dfe9dd4d801eefa80b84a16a1928
LucaVilotta <lucavilotta@hotmail.com>
2023-05-13 21:06:31 -0300

    me olvide en service

--- src/main/java/com/lvportfolio/argpro/Security/Controller/AuthController.java
+++ src/main/java/com/lvportfolio/argpro/Security/Controller/AuthController.java
@@ -27,7 +27,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-luca.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
